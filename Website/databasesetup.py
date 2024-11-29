3fad0d010dc8079aec50077e540ca799c35524fa
jitsolution19 <jitsolution19@gmail.com>
2024-11-29 11:34:33 +0530

    removed the flask project

--- Website/databasesetup.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-conn=sqlite3.connect("testingdata.db")
-print ("SQLite")
-# conn.execute("CREATE TABLE")
-conn.close
--- Website/static/contact.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('#contactusform').on('submit', function(){
-        alert('Thank You for sending Query. We will get back to you soon !!');
-    });
-});
-
-function sayHello() {
-    alert("Hello World")
-  }
